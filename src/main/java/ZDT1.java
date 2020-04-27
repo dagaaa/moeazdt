c00bae0c9b8d8da4665bb28e7d0d2cf75b1a0cb5
Paweł Matejko <p.matejko@smartrecruiters.com>
2020-04-27 10:49:06 +0200

    cześć

--- src/main/java/ZDT1.java
+++ src/main/java/ZDT1.java
@@ -1,31 +1,79 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.moeaframework.Executor;
 import org.moeaframework.analysis.plot.Plot;
 import org.moeaframework.core.NondominatedPopulation;
 import org.moeaframework.core.Solution;
 
+import static java.lang.System.currentTimeMillis;
+
 public class ZDT1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileWriter fileWriter = new FileWriter("results.csv");
+        fileWriter.write("problem, populationSize, maxEvaluations, elapsedTime [ms]\n");
+
         //configure and run this experiment
         String[] problems = {"ZDT1", "ZDT2", "ZDT3"};
+        int[] populationSizeArray = {10, 100, 1_000, 10_000};
+        int[] maxEvaluationsArray = {1_000, 10_000, 100_000};
+
         for (String problem : problems) {
-            NondominatedPopulation result = new Executor()
-                    .withProblem(problem)
-                    .withAlgorithm("NSGAII")
-                    .withMaxEvaluations(10000)
-                    .run();
+            for (int populationSize : populationSizeArray) {
+                for (int maxEvaluations : maxEvaluationsArray) {
+                    String label = String.format("Problem: %s, Population size: %s, Max evaluations: %s", problem, populationSize, maxEvaluations);
 
-            //display the results
-            System.out.println(problem);
-            System.out.format("Objective1  Objective2%n");
+                    Executor executor = new Executor()
+                            .withProblem(problem)
+                            .withAlgorithm("NSGAII")
+                            .withMaxEvaluations(10_000)
+                            .withProperty("populationSize", populationSize)
+                            .withProperty("maxEvaluations", maxEvaluations)
+                            .withProgressListener(progressEvent -> System.out.format(
+                                    "Label: %s, Elapsed time: %s, Remaining time: %s, Complete %s percent\n",
+                                    label,
+                                    progressEvent.getElapsedTime(),
+                                    progressEvent.getRemainingTime(),
+                                    progressEvent.getPercentComplete()
+                            ));
 
-            for (Solution solution : result) {
-                System.out.format("%.4f      %.4f%n",
-                        solution.getObjective(0),
-                        solution.getObjective(1));
+                    long before = currentTimeMillis();
+                    NondominatedPopulation result = executor.run();
+                    long elapsedTime = currentTimeMillis() - before;
+
+                    fileWriter
+                            .append(problem)
+                            .append(", ")
+                            .append(String.valueOf(populationSize))
+                            .append(", ")
+                            .append(String.valueOf(maxEvaluations))
+                            .append(", ")
+                            .append(String.valueOf(elapsedTime))
+                            .append("\n");
+//                    printResults(problem, result);
+
+                    new Plot()
+                            .add(label, result)
+                            .show();
+                }
             }
-            new Plot()
-                    .add(problem, result)
-                    .show();
+        }
+
+        fileWriter.flush();
+        fileWriter.close();
+    }
+
+    private static void printResults(String problem, NondominatedPopulation result) {
+        //display the results
+        System.out.println(problem);
+        System.out.format("Objective1  Objective2%n");
+
+        for (Solution solution : result) {
+            System.out.format(
+                    "%.4f      %.4f%n",
+                    solution.getObjective(0),
+                    solution.getObjective(1)
+            );
         }
     }
 }
