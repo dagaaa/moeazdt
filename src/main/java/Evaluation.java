b0897f82d6e4bf5220a2ada53eff965862bcc981
dagaaa <dagmaragoj@gmail.com>
2020-05-06 20:05:15 +0200

    fix SymPartSimple add SymPartRotate

--- src/main/java/Evaluation.java
+++ src/main/java/Evaluation.java
@@ -14,50 +14,55 @@ import static java.util.Arrays.asList;
 public class Evaluation {
     public static void main(String[] args) throws IOException {
         FileWriter fileWriter = new FileWriter("results.csv");
-        fileWriter.write("problem, populationSize, maxEvaluations, elapsedTime [ms]\n");
+        fileWriter.write("problem, algorithm, populationSize, maxEvaluations, elapsedTime [ms]\n");
 
         //configure and run this experiment
-        Collection<Class<? extends Problem>> problems = asList(SymPartSimple.class);
+        Collection<Class<? extends Problem>> problems = asList(SymPartRotate.class, SymPartSimple.class);
         int[] populationSizeArray = {10, 100, 1_000, 10_000};
         int[] maxEvaluationsArray = {1_000, 10_000, 100_000};
+        String[] algorithms = {"NSGAII", "GDE3", "eMOEA"};
 
         for (Class<? extends Problem> problem : problems) {
-            for (int populationSize : populationSizeArray) {
-                for (int maxEvaluations : maxEvaluationsArray) {
-                    String label = String.format("Problem: %s, Population size: %s, Max evaluations: %s", problem, populationSize, maxEvaluations);
+            for (String algorithm : algorithms) {
+                for (int populationSize : populationSizeArray) {
+                    for (int maxEvaluations : maxEvaluationsArray) {
+                        String label = String.format("Problem: %s, Algorithm: %s Population size: %s, Max evaluations: %s", problem,algorithm, populationSize, maxEvaluations);
 
-                    Executor executor = new Executor()
-                            .withProblemClass(problem)
-                            .withAlgorithm("NSGAII")
-                            .withMaxEvaluations(10_000)
-                            .withProperty("populationSize", populationSize)
-                            .withProperty("maxEvaluations", maxEvaluations)
-                            .withProgressListener(progressEvent -> System.out.format(
-                                    "Label: %s, Elapsed time: %s, Remaining time: %s, Complete %s percent\n",
-                                    label,
-                                    progressEvent.getElapsedTime(),
-                                    progressEvent.getRemainingTime(),
-                                    progressEvent.getPercentComplete()
-                            ));
+                        Executor executor = new Executor()
+                                .withProblemClass(problem)
+                                .withAlgorithm(algorithm)
+                                .withMaxEvaluations(10_000)
+                                .withProperty("populationSize", populationSize)
+                                .withProperty("maxEvaluations", maxEvaluations)
+                                .withProgressListener(progressEvent -> System.out.format(
+                                        "Label: %s, Elapsed time: %s, Remaining time: %s, Complete %s percent\n",
+                                        label,
+                                        progressEvent.getElapsedTime(),
+                                        progressEvent.getRemainingTime(),
+                                        progressEvent.getPercentComplete()
+                                ));
 
-                    long before = currentTimeMillis();
-                    NondominatedPopulation result = executor.run();
-                    long elapsedTime = currentTimeMillis() - before;
+                        long before = currentTimeMillis();
+                        NondominatedPopulation result = executor.run();
+                        long elapsedTime = currentTimeMillis() - before;
 
-                    fileWriter
-                            .append(problem.getSimpleName())
-                            .append(", ")
-                            .append(String.valueOf(populationSize))
-                            .append(", ")
-                            .append(String.valueOf(maxEvaluations))
-                            .append(", ")
-                            .append(String.valueOf(elapsedTime))
-                            .append("\n");
-                    printResults(problem.getSimpleName(), result);
+                        fileWriter
+                                .append(problem.getSimpleName())
+                                .append(", ")
+                                .append(algorithm)
+                                .append(", ")
+                                .append(String.valueOf(populationSize))
+                                .append(", ")
+                                .append(String.valueOf(maxEvaluations))
+                                .append(", ")
+                                .append(String.valueOf(elapsedTime))
+                                .append("\n");
+                        printResults(problem.getSimpleName(), result);
 
-                    new Plot()
-                            .add(label, result)
-                            .show();
+                        new Plot()
+                                .add(label, result)
+                                .show();
+                    }
                 }
             }
         }
--- src/main/java/SymPartRotate.java
+++ src/main/java/SymPartRotate.java
@@ -3,13 +3,15 @@ import org.moeaframework.core.variable.EncodingUtils;
 import org.moeaframework.core.variable.RealVariable;
 import org.moeaframework.problem.AbstractProblem;
 
-public class SymPartSimple extends AbstractProblem {
+public class SymPartRotate extends  AbstractProblem{
+
 
     private static final double a = 1;
     private static final double b = 10;
     private static final double c = 8;
+    private static final double w= Math.PI/4;
 
-    public SymPartSimple() {
+    public SymPartRotate() {
         super(2, 2);
     }
 
@@ -18,8 +20,11 @@ public class SymPartSimple extends AbstractProblem {
         double[] x = EncodingUtils.getReal(solution);
         double[] f = new double[numberOfObjectives];
 
-        double tDash1 = Math.signum(x[0]) * Math.ceil(((Math.abs(x[0]) - (a + (c / 2))) / ((2 * a) + c)));
-        double tDash2 = Math.signum(x[1]) * Math.ceil(((Math.abs(x[1]) - (b / 2)) / b));
+        double r1= (Math.cos(w)*x[0])-(Math.sin(w)*x[1]);
+        double r2= (Math.sin(w)*x[0])+(Math.cos(w)*x[1]);
+
+        double tDash1 = Math.signum(r1) * Math.ceil(((Math.abs(r1) - (a + (c / 2))) / ((2 * a) + c)));
+        double tDash2 = Math.signum(r2) * Math.ceil(((Math.abs(r2) - (b / 2)) / b));
 
         double t1 = Math.signum(tDash1) * Math.min(Math.abs(tDash1), 1.0);
         double t2 = Math.signum(tDash2) * Math.min(Math.abs(tDash2), 1.0);
@@ -27,8 +32,8 @@ public class SymPartSimple extends AbstractProblem {
         double p1 = x[0] - t1 * (c + 2 * a);
         double p2 = x[1] - t2 * b;
 
-         f[0] = Math.pow((p1 + a), 2) + Math.pow(p2, 2);
-         f[1] = Math.pow((p1 - a), 2) + Math.pow(p2, 2);
+        f[0] = Math.pow((p1 + a), 2) + Math.pow(p2, 2);
+        f[1] = Math.pow((p1 - a), 2) + Math.pow(p2, 2);
 
         solution.setObjectives(f);
 //        solution.setObjective(1, f2);
@@ -45,3 +50,4 @@ public class SymPartSimple extends AbstractProblem {
         return solution;
     }
 }
+
